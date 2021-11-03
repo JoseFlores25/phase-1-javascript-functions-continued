23a80065763add1589fb0d6c98fcaf374b3eb13e
Jose <flores.jose2185@gmail.com>
2021-11-03 17:59:44 -0400

    Work Done

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Your code here
+function saturdayFun(ride = "roller-skate") {
+  return `This Saturday, I want to ${ride}!`;
+}
+
+let mondayWork = function (work = "go to the office") {
+  return `This Monday, I will ${work}.`;
+};
+
+function wrapAdjective(visualFlair = "*") {
+  return function (adjective = "special") {
+    return `You are ${visualFlair}${adjective}${visualFlair}!`;
+  };
+}
